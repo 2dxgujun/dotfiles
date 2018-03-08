7274256df99b3f84abf7f4724586bef1c1cffde3
Jun Gu <2dxgujun@gmail.com>
2018-03-08 18:24:20 +0800

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-set -e
-
 DOTFILES=$(pwd -P)
+
+set -e
 
 if [ -w /home/linuxbrew ]; then
   BREW_HOME="/home/linuxbrew/.linuxbrew"
@@ -29,6 +29,24 @@ echo "> brew bundle" && brew bundle -v
 OVERWRITE_ALL=false
 BACKUP_ALL=false
 SKIP_ALL=false
+
+info () {
+  printf "\r  [ \033[00;34m..\033[0m ] $1\n"
+}
+
+user () {
+  printf "\r  [ \033[0;33m??\033[0m ] $1\n"
+}
+
+success () {
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+}
+
+fail () {
+  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+  echo ''
+  exit
+}
 
 link_file () {
   local src=$1 dst=$2
