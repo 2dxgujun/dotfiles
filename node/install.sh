4bc2058f5d4e1407ddedcf1dbcbd071ae917a130
Jun Gu <2dxgujun@gmail.com>
2018-04-02 13:15:09 +0800

    update install script

--- node/install.sh
+++ node/install.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-export NVM_DIR="$HOME/.nvm" && (
+export NVM_DIR="$HOME/.nvm" && [ ! -d "$NVM_DIR" ] && (
   git clone https://github.com/creationix/nvm.git "$NVM_DIR"
   cd "$NVM_DIR"
   git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" origin`
