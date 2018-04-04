3521de59929cb11835b243e65c326e997f58e4dc
Jun Gu <2dxgujun@gmail.com>
2018-04-04 12:27:08 +0800

    fix install script

--- vim/install.sh
+++ vim/install.sh
@@ -8,8 +8,11 @@ mkdir -p $HOME/.cache/vim/backup
 # Build ctags for tagbar plugin
 if ! type ctags > /dev/null; then
   BUILD_DIR="/tmp/ctags"
-  mkdir -p $BUILD_DIR && [ ! -d "$BUILD_DIR" ] && (
-    git clone https://github.com/universal-ctags/ctags.git "$BUILD_DIR"
-    cd "$BUILD_DIR"
-  ) && ./autogen.sh && ./configure && make && make install
+  mkdir -p $BUILD_DIR
+  git clone https://github.com/universal-ctags/ctags.git "$BUILD_DIR"
+  cd "$BUILD_DIR"
+  ./autogen.sh
+  ./configure
+  make
+  make install
 fi
